99ea63968cdf762a01f3fdc7bdf6b96c26590718
Luke D Harrison <lukedharrison19@gmail.com>
2022-03-12 19:04:24 +0000

    initial commit
    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const navBar = document.getElementById(`nav-bar`);
+const hamburgerIcon = document.getElementById(`hamburger-icon`);
+const mobileMenu = document.getElementById(`mobile-menu`);
+
+// get height of navbar
+const navHeight = navBar.getBoundingClientRect().height;
+
+hamburgerIcon.addEventListener(`click`, function () {
+  mobileMenu.classList.toggle(`hide`);
+  // set value of css variable to nav height
+  document.body.classList.toggle(`prevent-scroll`);
+  document.documentElement.style.setProperty(`--nav-height`, `${navHeight}px`);
+});
